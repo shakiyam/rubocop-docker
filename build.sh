c4a554933d916908dff6cb1dbedda05a2a4c6c3d
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-03-04 13:00:46 +0900

    Support podman

--- build.sh
+++ build.sh
@@ -3,13 +3,11 @@ set -eu -o pipefail
 
 readonly IMAGE_NAME='shakiyam/rubocop'
 
-cd "$(dirname "$0")"
-current_image="$(docker image ls -q $IMAGE_NAME:latest)"
-docker image build \
-  --build-arg http_proxy="${http_proxy:-}" \
-  --build-arg https_proxy="${https_proxy:-}" \
-  -t "$IMAGE_NAME" .
-latest_image="$(docker image ls -q $IMAGE_NAME:latest)"
+readonly DOCKER=$(command -v podman || command -v docker)
+current_image="$($DOCKER image ls -q $IMAGE_NAME:latest)"
+$DOCKER image build \
+  -t "$IMAGE_NAME" "$(dirname "$0")"
+latest_image="$($DOCKER image ls -q $IMAGE_NAME:latest)"
 if [[ "$current_image" != "$latest_image" ]]; then
-  docker image tag $IMAGE_NAME:latest $IMAGE_NAME:"$(date +%Y%m%d%H%S)"
+  $DOCKER image tag $IMAGE_NAME:latest $IMAGE_NAME:"$(date +%Y%m%d%H%S)"
 fi
--- update_lockfile.sh
+++ update_lockfile.sh
@@ -2,15 +2,23 @@
 set -eu -o pipefail
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
-
 [[ -e "$script_dir/Gemfile.lock" ]] || touch "$script_dir/Gemfile.lock"
-
-docker container run \
-  --rm \
-  -e http_proxy="${http_proxy:-}" \
-  -e https_proxy="${https_proxy:-}" \
-  -v "$script_dir/Gemfile":/work/Gemfile:ro \
-  -v "$script_dir/Gemfile.lock":/work/Gemfile.lock \
-  -u "$(id -u):$(id -g)" \
-  -w /work \
-  ruby:alpine sh -c 'HOME=/tmp bundle lock --update'
+if [[ $(command -v podman) ]]; then
+  podman container run \
+    --name update_lockfile$$ \
+    --rm \
+    --security-opt label=disable \
+    -v "$script_dir/Gemfile":/work/Gemfile \
+    -v "$script_dir/Gemfile.lock":/work/Gemfile.lock \
+    -w /work \
+    ruby:alpine sh -c 'HOME=/tmp bundle lock --update'
+else
+  docker container run \
+    --name update_lockfile$$ \
+    --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$script_dir/Gemfile":/work/Gemfile:ro \
+    -v "$script_dir/Gemfile.lock":/work/Gemfile.lock \
+    -w /work \
+    ruby:alpine sh -c 'HOME=/tmp bundle lock --update'
+fi

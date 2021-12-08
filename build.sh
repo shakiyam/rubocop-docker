bd2ad2c81aaa9dc5ed13f9708fc111134b281864
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-12-08 12:21:02 +0900

    Conform to shell style guide

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
 
-IMAGE_NAME='shakiyam/rubocop'; readonly IMAGE_NAME
-DOCKER=$(command -v podman || command -v docker); readonly DOCKER
-CURRENT_IMAGE="$($DOCKER image ls -q $IMAGE_NAME:latest)"; readonly CURRENT_IMAGE
+IMAGE_NAME='shakiyam/rubocop'
+readonly IMAGE_NAME
+DOCKER=$(command -v podman || command -v docker)
+readonly DOCKER
+CURRENT_IMAGE="$($DOCKER image ls -q $IMAGE_NAME:latest)"
+readonly CURRENT_IMAGE
 $DOCKER image build -t "$IMAGE_NAME" "$(dirname "$0")"
-LATEST_IMAGE="$($DOCKER image ls -q $IMAGE_NAME:latest)"; readonly LATEST_IMAGE
+LATEST_IMAGE="$($DOCKER image ls -q $IMAGE_NAME:latest)"
+readonly LATEST_IMAGE
 if [[ "$CURRENT_IMAGE" != "$LATEST_IMAGE" ]]; then
   $DOCKER image tag $IMAGE_NAME:latest $IMAGE_NAME:"$(date +%Y%m%d%H%S)"
 fi

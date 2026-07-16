a2dc161233020b7158690052eb2105a0b808ebc9
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2026-07-16 00:14:05 +0000

    Bump Ruby to 4.0.6

--- tools/update_lockfile.sh
+++ tools/update_lockfile.sh
@@ -6,7 +6,7 @@ readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/colored_echo.sh
 
-readonly RUBY_IMAGE="docker.io/library/ruby:4.0.5-slim-trixie"
+readonly RUBY_IMAGE="docker.io/library/ruby:4.0.6-slim-trixie"
 
 exit_flag=false
 while getopts ':e' flag; do
